630c16228d467bb18817c044d2a6bc1d69b5443d
Nathan McCarty <nathanalexmccarty@gmail.com>
2013-07-18 23:37:02 -0400

    Started to flush out enemy subsystem some more.

--- enemies.lisp
+++ enemies.lisp
@@ -2,7 +2,7 @@
   (:use :common-lisp :imas.bullets))
 
 ;; Basic class that all enemies extend from 
-(defclass enemy
+(defclass enemy ()
     ((x-pos :accessor get-x-pos
 	    :initarg :x-pos
 	    :initform 0)
@@ -15,6 +15,9 @@
      (health :accessor get-health
 	     :initarg :health
 	     :initform 1.0d0)
+     (max-health :accessor get-max-health
+		 :initarg :max-health
+		 :initform 100)
      (unclaimed-bullets :accessor get-unclaimed-bullets
 			:initarg :bullets
 			:initform nil)))
@@ -23,7 +26,7 @@
 ;; Required interface elements for enemy
 ;; step: Increases age by one tick and updates enemy state accordingly
 (defgeneric enemy-step (enemy)
-  (:docmentation "Advances state of enemy by one tick and returns enemy."))
+  (:documentation "Advances state of enemy by one tick and returns enemy."))
 ;; pos: returns a cons cell containing the position of the center of the enemy
 (defgeneric enemy-pos (enemy)
   (:documentation "Returns current position of enemy in form (x-pos . y-pos)."))
@@ -43,6 +46,53 @@
 ;; examine-health: Returns the health of this enemy as the ratio between its max health and current health
 (defgeneric enemy-examine-health (enemy)
   (:documentation "Returns the health of this enemy as ratio between max and current health."))
+;; real-health: Returns the actual ammount of health the enemy has
+(defgeneric enemy-real-health (enemy)
+  (:documentation "Returns the actual ammount of health enemy has, usually enemy-examine-health * enemy-max-health."))
 ;; damage: Deals ammount damage to enemy and updates state of enemy to reflect this 
 (defgeneric enemy-damage (enemy ammount)
   (:documentation "Damamges enemy by ammount."))
+
+
+;; Parts of the required interface that are implemented for you
+;; Simply conses the x-pos and y-pos, add your own method if you need something fancier
+(defmethod enemy-pos (enemy)
+  (cons (get-x-pos enemy) (get-y-pos enemy)))
+
+;; Simply returns the list in slot unclaimed bullets and flushes it
+(defmethod enemy-claim-bullets (enemy)
+  (let (list (get-unclaimed-bullets enemy))
+    (setf (get-unclaimed-bullets enemy) nil)
+    list))
+
+;; Simply returns what is in slot max-health
+(defmethod enemy-max-health (enemy)
+  (get-max-health enemy))
+
+;; Simply returns what is in slot health
+(defmethod enemy-examine-health (enemy)
+  (get-health enemy))
+
+;; Returns health * max-health
+(defmethod enemy-real-health (enemy)
+  (* (get-max-health enemy)
+     (get-health enemy)))
+
+;; Checks if the enemy is signifigantly out of bounds or has a health <= 0
+(defmethod enemy-cullable-p (enemy)
+  (or (<= (enemy-examine-health enemy) 0)
+      (> (get-x-pos enemy) 1.1)
+      (> (get-y-pos enemy) 1.1)
+      (< (get-x-pos enemy) -1.1)
+      (< (get-x-pos enemy) -1.1)))
+
+
+;; Parts of the required interface that aren't implemented for you
+(defmethod enemy-collision-p (enemy)
+  (error (concatenate 'string
+		      "Enemy-collision-p not defined for type:"
+		      (prin1-to-string (type-of enemy)))))
+(defmethod enemy-damage-p (enemy)
+  (error (concatenate 'string
+		      "Enemy-damage-p not defined for type:"
+		      (prin1-to-string (type-of enemy)))))
